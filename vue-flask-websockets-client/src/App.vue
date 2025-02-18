fd365cb4a3646d875914c2efef693949895153f9
katene30 <katenejack@gmail.com>
2025-02-19 10:26:59 +1300

    update score names with dynamic names

--- vue-flask-websockets-client/src/App.vue
+++ vue-flask-websockets-client/src/App.vue
@@ -9,21 +9,19 @@
       <input v-model="player2Name" placeholder="Enter Player 2 Name" class="px-4 py-2 bg-gray-700 text-white rounded-lg">
     </div>
 
-    <div>NAMES {{ player1Name }} & {{ player2Name }}</div>
-
     <!-- Pick Who Starts Serving -->
     <div class="mb-4 flex gap-4 text-2xl">
       <span>Who serves first?</span>
       <select v-model="selectedServer" @change="setInitialServer" class="px-4 py-2 bg-gray-700 text-white rounded-lg">
-        <option :value="1">Player 1</option>
-        <option :value="2">Player 2</option>
+        <option :value="1">{{ player1Name }}</option>
+        <option :value="2">{{ player2Name }}</option>
       </select>
     </div>
 
     <div class="flex justify-center w-full h-3/4 items-center gap-20">
       <!-- Player 1 -->
       <div class="text-center flex flex-col items-center w-1/3 p-6 rounded-xl bg-gray-800" :class="{ 'border-8 border-yellow-500 shadow-xl': server === 1  }">
-        <h2 class="text-4xl font-semibold">Player 1</h2>
+        <h2 class="text-4xl font-semibold">{{ player1Name }}</h2>
         <p class="text-[12rem] font-bold my-6">{{ scorePlayer1 }}</p>
         <div class="flex gap-4">
           <button @click="incrementScorePlayer1"
@@ -35,7 +33,7 @@
 
       <!-- Player 2 -->
       <div class="text-center flex flex-col items-center w-1/3 p-6 rounded-xl bg-gray-800" :class="{ 'border-8 border-yellow-500 shadow-xl': server === 2  }">
-        <h2 class="text-4xl font-semibold">Player 2</h2>
+        <h2 class="text-4xl font-semibold">{{ player2Name }}</h2>
         <p class="text-[12rem] font-bold my-6">{{ scorePlayer2 }}</p>
         <div class="flex gap-4">
           <button @click="incrementScorePlayer2"
